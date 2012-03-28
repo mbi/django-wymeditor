d3e7ae985f2811ef37a9378edfb7f2a887494369
Marco Bonetti <mbonetti@gmail.com>
2012-03-28 16:36:49 +0200

    laod jquery-ui and jquery CDN protocol-independently

--- wymeditor/widgets.py
+++ wymeditor/widgets.py
@@ -2,6 +2,7 @@ from django import forms
 from django.conf import settings
 from django.contrib.admin import widgets
 
+
 class WYMEditorArea(forms.Textarea):
     def __init__(self, attrs={}):
         base_class = attrs.get("class", "")
@@ -11,15 +12,16 @@ class WYMEditorArea(forms.Textarea):
     class Media:
         css = {
             "all": (
-                "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/smoothness/jquery-ui.css",
+                "//ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/smoothness/jquery-ui.css",
                 '%swymeditor/skins/twopanels/skin.css' % settings.STATIC_URL,
             )
         }
         js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
+            '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
             '%swymeditor/jquery.wymeditor.js' % settings.STATIC_URL,
             '%sjs/load_wymeditor.js' % settings.STATIC_URL,
         )
+
 
 class AdminWYMEditorArea(widgets.AdminTextareaWidget, WYMEditorArea):
     pass
