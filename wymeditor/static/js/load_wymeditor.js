0da0f299bb3bb57fbc1ea9ce972b14909b0f1405
Marco Bonetti <mbonetti@gmail.com>
2012-03-28 17:26:17 +0200

    Better style integration in the admin, fix for broken plugin actions

--- wymeditor/static/js/load_wymeditor.js
+++ wymeditor/static/js/load_wymeditor.js
@@ -1,32 +1,8 @@
 (function ($) {
     $(function () {
-        wymeditor_filebrowser = function(wym, wdw) {
-            // Django filebrowser URL, #FIXME -- depends on URLconf
-            var fb_url = '/admin/filebrowser/browse/',
-                dlg = $(wdw.document.body);
-
-            if (dlg.hasClass('wym_dialog_image')) {
-                // this is an image dialog
-                dlg.find('.wym_src')
-                    .css('width', '200px')
-                    .attr('id', 'filebrowser')
-                    .after('<a id="fb_link" title="Filebrowser" href="#">Filebrowser</a>');
-                dlg.find('fieldset')
-                    .append('<a id="link_filebrowser"><img id="image_filebrowser" /></a><br /><span id="help_filebrowser"></span>');
-                dlg.find('#fb_link').click(function() {
-                    fb_window = wdw.open(
-                        fb_url + '?pop=1',
-                        'filebrowser',
-                        'height=600,width=840,resizable=yes,scrollbars=yes'
-                    );
-                    fb_window.focus();
-                    return false;
-                });
-          }
-        };
-
+        var base_path = "/static/wymeditor/";
         $(".WYMEditor").wymeditor({
-            basePath: "/static/wymeditor/",
+            basePath: base_path,
             updateSelector: "input:submit",
             updateEvent: "click",
             //postInitDialog: wymeditor_filebrowser,
@@ -37,7 +13,29 @@
                 {'name': 'caption', 'title': 'P: Caption', 'expr': 'p'},
                 {'name': 'align-left', 'title': 'Float: Left', 'expr': 'p, div, img'},
                 {'name': 'align-right', 'title': 'Float: Right', 'expr': 'p, div, img'}
-            ]
+            ],
+            wymPath: base_path + 'jquery.wymeditor.js',
+            skinPath: base_path + 'skins/django/',
+            jQueryPath: '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
+            dialogHtml: String() +
+                '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" ' +
+                        '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">' +
+                '<html dir="' + WYMeditor.DIRECTION + '">' +
+                    '<head>' +
+                        '<link rel="stylesheet" type="text/css" href="'+window.__admin_media_prefix__+'css/base.css" />' +
+                        '<link rel="stylesheet" type="text/css" href="'+window.__admin_media_prefix__+'css/forms.css" />' +
+                        '<style type="text/css"> .wym_dialog fieldset { border: 1px solid #EEF; } </style>' +
+                        '<link rel="stylesheet" type="text/css" media="screen" ' +
+                            'href="' + WYMeditor.CSS_PATH + '" />' +
+                        '<title>' + WYMeditor.DIALOG_TITLE + '</title>' +
+                        '<script type="text/javascript" ' +
+                            'src="' + WYMeditor.JQUERY_PATH + '"></script>' +
+                        '<script type="text/javascript" ' +
+                            'src="' + WYMeditor.WYM_PATH + '"></script>' +
+                    '</head>' +
+                    WYMeditor.DIALOG_BODY +
+                '</html>'
+
         });
     });
 })(jQuery);
